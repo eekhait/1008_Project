806b57e134b06bc93e27af5d1e27d860c71a4b24
cradlesong-castle <55814928+cradlesong-castle@users.noreply.github.com>
2020-03-06 16:33:20 +0800

    Cleaned up some code so as to trigger less PyCharm underlines. setCoordinates renamed to create_edges_json listToStr to list_to_str Added more comments on array to be passed ([lat,long],...) removed 'overlay' variable and instead references json file directly

--- map.py
+++ map.py
@@ -1,50 +1,41 @@
 import folium
-import os
-import json
 import io
 import sys
 from PyQt5 import QtWidgets, QtWebEngineWidgets
 
-def setCoordinates(array):
-    f = open("overlay.json", "w") 
-    listToStr = ', '.join([str(elem) for elem in array]) #join elements of array with comma
-    f.write("{\"type\": \"FeatureCollection\", \"features\": [ { \"type\": \"Feature\", \"properties\": {}, \"geometry\": { \"type\": \"LineString\", \"coordinates\": [")
-    f.write(listToStr)
+
+def create_edges_json(array):
+    # This function creates a .json file to draw edges between nodes.
+    # The array it receives should be in the format -> [[lat,long],...,[lat,long]]
+    f = open("overlay.json", "w")   # 'w' will overwrite any existing content
+    list_to_str = ', '.join([str(elem) for elem in array]) # join elements of array with comma
+    f.write("{\"type\": \"FeatureCollection\", \"features\": [ { \"type\": \"Feature\", \"properties\": {}, "
+            "\"geometry\": { \"type\": \"LineString\", \"coordinates\": [")
+    f.write(list_to_str)
     f.write("]}}]}")
     f.close()
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-
-    data = io.BytesIO()
-    
     # Create map object, set default location, map theme & zoom
-    m = folium.Map(location=[1.4046357,103.9090000],tiles="Stamen Toner", zoom_start=14.5)
-
+    m = folium.Map(location=[1.4046357, 103.9090000], tiles="Stamen Terrain", zoom_start=14.5)
     # Global tooltip, hover info
     tooltip = 'Click For More Info'
 
-    # Add coordinates to json file
     # Static Array, pending data of routes
-    array = [[103.899582, 1.405146],[103.904666, 1.414444],[103.907902, 1.402316],[103.918416, 1.403702],[103.913274, 1.394577]]
-    # Call function and append new route data
-    setCoordinates(array)
-
-    # Location Data from json
-    overlay = os.path.join('data', 'overlay.json')
-
-    # Loop to repackage from [lat, long] to [long, lat]
-    for i in range(0, len(array)):  
+    testArray = [[103.899582, 1.405146], [103.904666, 1.414444]]
+    create_edges_json(testArray)            # calls function to create json file based on coordinates given
+    for i in range(0, len(testArray)):      # creates markers on map based on coordinates given
         # Set coordinates location, popup information, tooltip color etc
-        folium.Marker([array[i][1],array[i][0]],
-                popup= i,
-                tooltip=tooltip).add_to(m)
+        folium.Marker([testArray[i][1], testArray[i][0]],   # function works in reverse order of [long,lat]
+                      popup=i, tooltip=tooltip).add_to(m)
 
-    # Geojson overlay
-    folium.GeoJson(overlay, name='Punggol').add_to(m)   # line plotting
+    # GeoJson overlay
+    folium.GeoJson('overlay.json', name='Punggol').add_to(m)   # line plotting
 
+    # Initialization of the map
+    data = io.BytesIO()
     m.save(data, close_file=False)
-
     w = QtWebEngineWidgets.QWebEngineView()
     w.setHtml(data.getvalue().decode())
     w.resize(840, 680)
